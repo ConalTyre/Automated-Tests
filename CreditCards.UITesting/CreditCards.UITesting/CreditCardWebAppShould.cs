aea00af6c48a2310fc7965627a9b2c864f74721b
ConalTyre <51913316+ConalTyre@users.noreply.github.com>
2020-08-11 15:35:45 +0100

    Add files via upload

---
+++ CreditCards.UITesting/CreditCards.UITesting/CreditCardWebAppShould.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xunit;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+
+namespace CreditCards.UITesting
+{
+    public class CreditCardWebAppShould
+    {
+        [Fact]
+        [Trait("Category", "Smoke")]
+        public void LoadApplicationPage()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl("http:\\www.FoodsConnected.com");
+            }
+
+        }
+    }
+}
